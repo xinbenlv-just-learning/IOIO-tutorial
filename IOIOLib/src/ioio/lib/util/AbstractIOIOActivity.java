20f62f643192b4084378365825d880aaafba6cfd
Zainan Victor Zhou <xinbenlv@gmail.com>
2011-08-30 22:23:45 -0400

    init

---
+++ IOIOLib/src/ioio/lib/util/AbstractIOIOActivity.java
@@ -0,0 +1,179 @@
+package ioio.lib.util;
+
+import ioio.lib.api.IOIO;
+import ioio.lib.api.IOIOFactory;
+import ioio.lib.api.exception.ConnectionLostException;
+import ioio.lib.api.exception.IncompatibilityException;
+import android.app.Activity;
+import android.util.Log;
+
+/**
+ * A convenience class for easy creation of IOIO-based applications.
+ * 
+ * It is used by creating a concrete Activity in your application, which extends
+ * this class. This class then takes care of proper creation and abortion of the
+ * IOIO connection and of a dedicated thread for IOIO communication.
+ * 
+ * In the intended usage the client should extend this class and implement
+ * {@link #createIOIOThread()}, which should return an implementation of the
+ * IOIOThread abstract class. In this implementation, the client implements the
+ * {@link IOIOThread#setup()} method, which gets called as soon as communication
+ * with the IOIO is established, and the {@link IOIOThread#loop()} method, which
+ * gets called repetitively as long as the IOIO is connected. Both methods
+ * should access the {@link IOIOThread#ioio_} field for controlling the IOIO.
+ * 
+ */
+public abstract class AbstractIOIOActivity extends Activity {
+	private IOIOThread ioio_thread_;
+
+	/**
+	 * Subclasses should call this method from their own onResume() if
+	 * overloaded. It takes care of connecting with the IOIO.
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		ioio_thread_ = createIOIOThread();
+		ioio_thread_.start();
+	}
+
+	/**
+	 * Subclasses should call this method from their own onPause() if
+	 * overloaded. It takes care of disconnecting from the IOIO.
+	 */
+	@Override
+	protected void onPause() {
+		super.onPause();
+		ioio_thread_.abort();
+		try {
+			ioio_thread_.join();
+		} catch (InterruptedException e) {
+		}
+	}
+
+	/**
+	 * Subclasses should implement this method by returning a concrete subclass
+	 * of {@link IOIOThread}.
+	 * 
+	 * @return An implementation of {@link IOIOThread}
+	 */
+	protected abstract IOIOThread createIOIOThread();
+
+	/**
+	 * An abstract class, which facilitates a thread dedicated for IOIO
+	 * communication.
+	 */
+	protected abstract class IOIOThread extends Thread {
+		/** Subclasses should use this field for controlling the IOIO. */
+		protected IOIO ioio_;
+		private boolean abort_ = false;
+		private boolean connected_ = true;
+
+		/** Not relevant to subclasses. */
+		@Override
+		public final void run() {
+			super.run();
+			while (true) {
+				try {
+					synchronized (this) {
+						if (abort_) {
+							break;
+						}
+						ioio_ = IOIOFactory.create();
+					}
+					ioio_.waitForConnect();
+					connected_ = true;
+					setup();
+					while (!abort_) {
+						loop();
+					}
+					ioio_.disconnect();
+				} catch (ConnectionLostException e) {
+					if (abort_) {
+						break;
+					}
+				} catch (InterruptedException e) {
+					ioio_.disconnect();
+					break;
+				} catch (IncompatibilityException e) {
+					Log.e("AbstractIOIOActivity",
+							"Incompatible IOIO firmware", e);
+					incompatible();
+					// nothing to do - just wait until physical disconnection
+					try {
+						ioio_.waitForDisconnect();
+					} catch (InterruptedException e1) {
+						ioio_.disconnect();
+					}
+				} catch (Exception e) {
+					Log.e("AbstractIOIOActivity",
+							"Unexpected exception caught", e);
+					ioio_.disconnect();
+					break;
+				} finally {
+					try {
+						if (ioio_ != null) {
+							ioio_.waitForDisconnect();
+							if (connected_) {
+								disconnected();
+							}
+						}
+					} catch (InterruptedException e) {
+					}
+				}
+			}
+		}
+
+		/**
+		 * Subclasses should override this method for performing operations to
+		 * be done once as soon as IOIO communication is established. Typically,
+		 * this will include opening pins and modules using the openXXX()
+		 * methods of the {@link #ioio_} field.
+		 */
+		protected void setup() throws ConnectionLostException,
+				InterruptedException {
+		}
+
+		/**
+		 * Subclasses should override this method for performing operations to
+		 * be done repetitively as long as IOIO communication persists.
+		 * Typically, this will be the main logic of the application, processing
+		 * inputs and producing outputs.
+		 */
+		protected void loop() throws ConnectionLostException,
+				InterruptedException {
+			sleep(100000);
+		}
+
+		/**
+		 * Subclasses should override this method for performing operations to
+		 * be done once as soon as IOIO communication is lost or closed.
+		 * Typically, this will include GUI changes corresponding to the change.
+		 * This method will only be called if setup() has been called.
+		 * The {@link #ioio_} member must not be used from within this method.
+		 */
+		protected void disconnected() throws InterruptedException {
+		}
+
+		/**
+		 * Subclasses should override this method for performing operations to
+		 * be done if an incompatible IOIO firmware is detected.
+		 * The {@link #ioio_} member must not be used from within this method.
+		 * This method will only be called once, until a compatible IOIO is
+		 * connected (i.e. {@link #setup()} gets called).
+		 */
+		protected void incompatible() {
+		}
+
+		/** Not relevant to subclasses. */
+		public synchronized final void abort() {
+			abort_ = true;
+			if (ioio_ != null) {
+				ioio_.disconnect();
+			}
+			if (connected_) {
+				interrupt();
+			}
+		}
+	}
+}
---
+++ applications/HelloIOIOUart/src/ioio/examples/hello/MainActivity.java
@@ -0,0 +1,163 @@
+package ioio.examples.hello;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import ioio.examples.hello.R;
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.Uart;
+import ioio.lib.api.exception.ConnectionLostException;
+import ioio.lib.util.AbstractIOIOActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+/**
+ * This is the main activity of the HelloIOIO example application.
+ * 
+ * It displays a toggle button on the screen, which enables control of the
+ * on-board LED. This example shows a very simple usage of the IOIO, by using
+ * the {@link AbstractIOIOActivity} class. For a more advanced use case, see the
+ * HelloIOIOPower example.
+ */
+public class MainActivity extends AbstractIOIOActivity {
+	private ToggleButton button_;
+	private TextView textview_;
+	public static Handler mHandler;
+	/**
+	 * Called when the activity is first created. Here we normally initialize
+	 * our GUI.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		textview_ = (TextView) findViewById(R.id.title);
+		
+        mHandler=new Handler(){
+        	public void handleMessage(Message msg)
+        	{
+        		String s="";
+        		switch (msg.what){
+        			case 0x101:
+        				s= "num:"+msg.obj.toString()+"\n";
+        				break;
+        			case 0x102:
+        				s= "received:"+msg.obj.toString()+"\n";
+        		}
+				MainActivity.this.textview_.append(s);
+				
+        		super.handleMessage(msg);
+        	}
+        };
+		
+	}
+
+	/**
+	 * This is the thread on which all the IOIO activity happens. It will be run
+	 * every time the application is resumed and aborted when it is paused. The
+	 * method setup() will be called right after a connection with the IOIO has
+	 * been established (which might happen several times!). Then, loop() will
+	 * be called repetitively until the IOIO gets disconnected.
+	 */
+	class IOIOThread extends AbstractIOIOActivity.IOIOThread {
+		/** The on-board LED. */
+		private DigitalOutput led_;
+		private Uart uart;
+		private InputStream in;
+		private OutputStream out;
+		private byte[] wbuf={'h','e','l','l','o'};
+		private boolean reading=false;//false
+		/**
+		 * Called every time a connection with IOIO has been established.
+		 * Typically used to open pins.
+		 * 
+		 * @throws ConnectionLostException
+		 *             When IOIO connection is lost.
+		 * 
+		 * @see ioio.lib.util.AbstractIOIOActivity.IOIOThread#setup()
+		 */
+		@Override
+		protected void setup() throws ConnectionLostException {
+			led_ = ioio_.openDigitalOutput(0, true);
+			uart = ioio_.openUart(37, 38, 38400, Uart.Parity.NONE,Uart.StopBits.ONE );
+			in = uart.getInputStream();
+			out = uart.getOutputStream();
+			
+		}
+
+		/**
+		 * Called repetitively while the IOIO is connected.
+		 * 
+		 * @throws ConnectionLostException
+		 *             When IOIO connection is lost.
+		 * @throws IOException 
+		 * 
+		 * @see ioio.lib.util.AbstractIOIOActivity.IOIOThread#loop()
+		 */
+		@Override
+		protected void loop() throws ConnectionLostException {
+			
+			if(reading==false)
+			{
+				
+				try {
+					out.write(wbuf);
+					out.flush();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					
+				}
+				reading=true;
+				led_.write(true);
+			}
+			else{
+				try {
+				int availableBytes =in.available(); 
+				Message msg =new Message();
+				msg.what=0x101;
+				msg.obj=availableBytes;
+				MainActivity.mHandler.sendMessage(msg);
+				if(availableBytes>0){
+				byte[] rbuf=new byte[100];
+				
+					in.read(rbuf,0,availableBytes);
+					char[] temp = (new String(rbuf,0,availableBytes)).toCharArray();
+					String temp2=new String(temp);
+					Message msg1 =new Message();
+					msg1.what=0x102;
+					msg1.obj=temp2;
+					MainActivity.mHandler.sendMessage(msg1);
+				}
+				reading=false;
+				led_.write(false);			
+				}
+				catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					
+				}
+			}
+			
+			try {
+				sleep(300);
+			} 
+				catch (InterruptedException e) {
+			}
+		}
+	}
+
+	/**
+	 * A method to create our IOIO thread.
+	 * 
+	 * @see ioio.lib.util.AbstractIOIOActivity#createIOIOThread()
+	 */
+	@Override
+	protected AbstractIOIOActivity.IOIOThread createIOIOThread() {
+		return new IOIOThread();
+	}
+}
